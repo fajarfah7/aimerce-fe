ead8eef4e930c00ee7430233c13c4b2737812d7f
Fajar Fahrurozi <fajar.fah7@gmail.com>
2026-03-08 22:34:43 +0700

    feat: initial commit

---
+++ app/auth/login/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <div>
+      Username: <input type="text" />
+      Password: <input type="password" />
+      <button>Login</button>
+    </div>
+  );
+}
